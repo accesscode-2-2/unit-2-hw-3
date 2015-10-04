0d353819accf2b1b609377cd6f2f8a6536d63c22
Chrisrdavid7 <chrisrdavid7@gmail.com>
2015-10-04 17:43:48 -0400

    Third commit

---
+++ MarsWater/MarsWater/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MarsWater
+//
+//  Created by Chris David on 10/4/15.
+//  Copyright © 2015 Chris David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
