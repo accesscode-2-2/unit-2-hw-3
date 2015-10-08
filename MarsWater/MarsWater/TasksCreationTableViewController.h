ad085312fa3cb7254d6e94633a58440253582f6a
Umar <umahmud@outlook.com>
2015-10-08 17:44:03 -0400

    made some progress

---
+++ MarsWater/MarsWater/TasksCreationTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TasksCreationTableViewController.h
+//  MarsWater
+//
+//  Created by Umar on 10/8/15.
+//  Copyright © 2015 Michael Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TasksCreationTableViewController : UITableViewController
+
+@end
---
+++ MarsWater/MarsWater/TasksTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TasksTableViewController.h
+//  MarsWater
+//
+//  Created by Umar on 10/8/15.
+//  Copyright © 2015 Michael Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "List.h"
+
+@interface TasksTableViewController : UITableViewController
+
+@property (nonatomic) List *list;
+
+@end
