780e982cb766b661f8fb4d52a68d140166e3d05c
charles <ckangkitchen@gmail.com>
2015-10-08 16:46:42 -0400

    screen is black when clicking on list item, is very choppy too. says add new task to null, will try to figure out what's going on

--- MarsWater/MarsWater/TaskCreationTableViewController.m
+++ MarsWater/MarsWater/TaskCreationTableViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "TaskCreationTableViewController.h"
+#import "Task.h"
+#import "AppDelegate.h"
 
 @interface TaskCreationTableViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *createTaskTextField;
+@property (nonatomic) Task *task;
+@property (nonatomic) NSMutableOrderedSet *listTasks;
 
 @end
 
@@ -18,82 +24,57 @@
     [super viewDidLoad];
     self.navigationItem.title = @"Create Tasks";
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    self.listTasks = self.list.task.mutableCopy;
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
-}
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    [self setupNavigationBar];
     
-    // Configure the cell...
+    AppDelegate *delegate = [UIApplication sharedApplication].delegate;
     
-    return cell;
+    self.task = [NSEntityDescription insertNewObjectForEntityForName:@"Task" inManagedObjectContext:delegate.managedObjectContext];
+    
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+-(void)setupNavigationBar{
+    
+    self.navigationItem.title = [NSString stringWithFormat:@"Add New Task to %@", self.list.title];
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancel)];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(save)];
+    
+    self.navigationItem.rightBarButtonItem.tintColor = [UIColor lightGrayColor];
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+-(void)cancel{
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+-(void)save{
+    
+    // set the task properties
+    
+    self.task.taskDescription = self.createTaskTextField.text;
+    
+    if (self.task.createdAt == nil) {
+        
+        self.task.createdAt = [NSDate date];
+        
+    }else {
+        
+        self.task.updatedAt = [NSDate date];
+    }
+    
+    
+  // update lisk task throwing yellow flag?
+    
+    self.list.task = self.listTasks;
+    
+    AppDelegate *delegate = [UIApplication sharedApplication].delegate;
+    
+    [delegate.managedObjectContext save:nil];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
 }
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
